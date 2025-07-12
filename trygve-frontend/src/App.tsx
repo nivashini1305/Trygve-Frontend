7cb9b02c88230794ab83f42d9a5b00f296f83a65
nivashini1305 <nivashinigovindaraj05@gmail.com>
2025-07-12 11:00:31 +0530

    Initial commit

---
+++ trygve-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="hero-container">
+      <div className="overlay" />
+      <div className="hero-content">
+          <h1 className="logo-text">Trygve</h1>
+          <p className="subtitle">Trusted Guardian of Life</p>
+        </div>
+      </div>
+  )
+}
+
+export default App
